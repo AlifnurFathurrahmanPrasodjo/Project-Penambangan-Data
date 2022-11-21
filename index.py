4d936146e8b4bd1a4549719838ed256ae040ab1e
AlifnurFathurrahmanPrasodjo <111824164+AlifnurFathurrahmanPrasodjo@users.noreply.github.com>
2022-11-22 00:49:37 +0700

    Add files via upload

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import norm
 import time
+import webbrowser
 
 # pige title
 st.set_page_config(
@@ -25,9 +26,20 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 
 # insialisasi web
-kolom = st.columns((2, 0.48, 2.7))
-home = kolom[1].button('Home')
-about = kolom[2].button('About')
+kolom = st.columns((0.1, 3, 1, 3, 1.3))
+GH = 'https://github.com/AlifnurFathurrahmanPrasodjo/Project-Penambangan-Data.git'
+
+if kolom[1].button('GitHub'):
+    webbrowser.open_new_tab(GH)
+
+home = kolom[2].button('Home')
+about = kolom[3].button('About')
+
+JT = 'https://alifnurfathurrahmanprasodjo.github.io/DATAMINING/project_pendat.html?highlight=project'
+
+if kolom[4].button('Jupyter'):
+    webbrowser.open_new_tab(JT)
+
 
 # home page
 if home==False and about==False or home==True and about==False:
@@ -44,8 +56,8 @@ if home==False and about==False or home==True and about==False:
     with col3:
         Number_children = st.number_input("Jumlah anak yang dimiliki",min_value=0,max_value=999999999999)
     with col4:
-        education_level = st.number_input("Level edukasi (1-19)",min_value=0,max_value=999999999999)
-    with col3:
+        education_level = st.number_input("Level edukasi",min_value=0,max_value=999999999999)
+    with col5:
         total_members = st.number_input("Jumlah anggota keluarga",min_value=0,max_value=999999999999)
     incoming_salary = st.selectbox("Apakah memiliki pendapatan?",('Ya','Tidak'))
     #    Centering Butoon 
@@ -60,6 +72,9 @@ if home==False and about==False or home==True and about==False:
         else:
             sex = 1
         
+        # cek memiliki pendapatan
+        #0 = Ya
+        #1 = Tidak
         if incoming_salary == 'Ya':
             incoming_salary = 0
         else:
@@ -71,20 +86,25 @@ if home==False and about==False or home==True and about==False:
         # cek prediksi
         with st.spinner("Tunggu Sebentar Masih Proses..."):
             if prediksi[-1] == 0:
+                #st.write(prediksi)
                 time.sleep(1)
                 st.success("Hasil Prediksi : "+nama+", anda tidak depresi")
             else:
+                #st.write(prediksi)
                 time.sleep(1)
                 st.warning("Hasil Prediksi : "+nama+", anda depresi")
 
 # about page
 if about==True and home==False:
     st.markdown("<h1 style='text-align: center; color: white; margin:0 ; padding:0;'>Tentang Sistem ini</h1>", unsafe_allow_html=True)
+    st.write(' ')
     st.write('Sistem Predeksi Depresi adalah sebuah sistem yang bertujuan untuk memprediksi apakah seseorang dalam keadaan depresi atau tidak. Sistem ini dibuat menggunakan bahasa pemrograman python dan library streamlit.')
-    st.markdown("<p  color: white;'>Pada sistem ini menggunakan model KNN ( <i>K-nearest neighbors algorithm</i> ) dengan parameter <b>K = 6</b> . Dataset yang digunakan memiliki <b>8 fitur</b> termasuk kelas.</p>", unsafe_allow_html=True)
-    st.write('Alasan menggunakan model KNN dengan parameter k = 6 adalah karena memiliki akurasi yang terbesar dari model lainnya pada dataset ini, sehingga diputuskan untuk menggunakan model tersebut. Sebelumnya sudah dilakukan dua analisa dan percobaan untuk model lainnya, lebih lengkapnya pada link dibawah.')
-    st.markdown("<p> <a class='text-warning' target='_blank' href=''>Percobaan model pertama</a> | <a target='_blank' class='text-warning' href=''>Percobaan model kedua</a></p>", unsafe_allow_html=True)
+    st.markdown("<p  color: white;>Pada sistem ini menggunakan model KNN ( <i>K-nearest neighbors algorithm</i> ) dengan parameter <b>K = 6</b> . Dataset yang digunakan memiliki <b>8 fitur</b> termasuk kelas. Alasan menggunakan model KNN dengan parameter k = 6 adalah karena memiliki akurasi yang terbesar dari model lainnya pada dataset ini, sehingga diputuskan untuk menggunakan model tersebut.</p>", unsafe_allow_html=True)
+    st.info('Pada data input level edukasi, Level 0 = tidak beredukasi||Level 1-6 = SD kelas 1-6||Level 7-9 = SMP kelas 7-9||Level 10-12 = SMA kelas 10-12||Level 13-14 = S1||Level 15-16 = S2||Level 17-19 = S3', icon="ℹ️")
+    st.caption('Alifnur Fathurrahman Prasodjo - 200411100150')
+
     
+
 
         
 
